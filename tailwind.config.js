706f2885a594c69e2b988f68dfa37202042309f2
Pablo Gimenez <pablo.gimenez@mediamonks.com>
2021-12-31 20:28:56 -0300

    Update dependencies packages to the latest one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       display: ["Rambla"],
